369761a691dc37a174226dcd627676ce7fa3f9bb
906284 <michaelcounseller@gmail.com>
2022-03-20 13:17:06 -0500

    installed materialdesignicons for react, created sidebar, created objects for the side bar, installed react router for later

---
+++ src/components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Icon from '@mdi/react'
+import { mdiMenu } from '@mdi/js'
+
+const Sidebar = () => {
+    return (
+        <>
+        <nav>
+            <p>highiehgi</p>
+            <div>
+                <Icon path={mdiMenu}/>
+            </div>
+        </nav>
+        </>
+    )
+};
+
+export default Sidebar;
