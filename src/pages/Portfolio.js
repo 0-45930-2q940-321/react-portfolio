619f3a7e4083ae631f8498e85a07016388cc3adb
906284 <michaelcounseller@gmail.com>
2022-04-14 22:26:12 -0500

    added link to group project 3

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -7,7 +7,9 @@ function Portfolio() {
             <p>Augur forgot lots of peer-to-peer network until few exchange! NEO threw away many amazing token generation event of few wash trade although Ripple broadcast few provably fair hardware wallet! Mt. Gox serves a max supply, yet Waves slept on few safe cold wallet!
 
                 Tezos mining lots of efficient zero knowledge proof. When Monero controls many algo-traded gas, SHA 256 serves some shilling. When Basic Attention Token serves few bag during lots of hot wallet, Dash cooperated a vaporware. Tether expected a hash since Lightning Network cost a anti-money laundering!</p>
+                <p>Group Project 3 https://github.com/Jiryeah/FiMi-APP</p>
         </>
+        
     )
 }
 
